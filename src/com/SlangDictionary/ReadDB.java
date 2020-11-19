13f090b24b7868f6cf176f57a76ab12b4ea1fe16
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2020-11-19 13:43:12 +0700

    reading input function and storing it to hash map

--- src/com/SlangDictionary/ReadDB.java
+++ src/com/SlangDictionary/ReadDB.java
@@ -4,21 +4,52 @@ package com.SlangDictionary;/*
 */
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ReadDB {
-    static final String FILE_PATH = "../../assets/files/base.txt";
-    public static void main(String[] args) {
+    public static final String FILE_PATH = "./assets/files/base.txt";
+    public static HashMap<String, String> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
         FileInputStream inputStream = null;
         Scanner sc = null;
-
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try{
             inputStream = new FileInputStream(FILE_PATH);
             sc = new Scanner(inputStream, "UTF-8");
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                String[] splited = line.split("`", 0);
 
+                if (splited.length != 2){
+                    System.out.println(Arrays.toString(splited));
+                    System.out.println(line);
+                }
+                else{
+                    map.put(splited[0], splited[1]);
+                }
+            }
+
+            if (sc.ioException() != null) {
+                throw sc.ioException();
+            }
         }
         catch (Exception error){
-
+            System.out.print(error.getMessage());
         }
+        finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+        Scanner inputSc = new Scanner(System.in);
+        String slang = inputSc.nextLine();
+        System.out.print(map.get(slang));
     }
 }
